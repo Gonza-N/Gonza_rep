c1f4a6da9026c05b36ceeb322535548f0b3376f7
Gonzalo Narváez Arellano <gonzalo.liceo2001@gmail.com>
2024-09-29 00:03:39 -0300

    version actualizada y funcionando

--- Principios/servidor.py
+++ Principios/servidor.py
@@ -1,18 +1,26 @@
 import json
 import socket
 import threading
+from collections import deque
 
-def cargar_datos():
+def guardar_db():
+    with open('clientes_db.json', 'w') as f:
+        json.dump(clientes_db, f)
+
+def cargar_db():
     try:
-        with open('clientes_db.json', 'r') as archivo:
-            return json.load(archivo)
+        with open('clientes_db.json', 'r') as f:
+            return json.load(f)
     except FileNotFoundError:
-        print("Archivo de base de datos no encontrado, creando una nueva base de datos vacía.")
-        return {}
+        return {
+            "gonzalo@gmail.com": {"password": "1234", "historial_compras": []},
+            "ammi@gmail.com": {"password": "5678", "historial_compras": []}
+        }
 
-clientes_db = cargar_datos()
+clientes_db = cargar_db()
+clientes_conectados = {}
+cola_espera = deque()
 
-# Manejo de conexiones de clientes
 def manejar_cliente(cliente_socket, direccion):
     email = cliente_socket.recv(1024).decode().strip()
     if email in clientes_db:
@@ -20,21 +28,22 @@ def manejar_cliente(cliente_socket, direccion):
         password = cliente_socket.recv(1024).decode().strip()
         if clientes_db[email]["password"] == password:
             cliente_socket.send("Autenticación exitosa.".encode())
+            clientes_conectados[email] = cliente_socket
             manejar_sesiones(cliente_socket, email)
         else:
             cliente_socket.send("Error: Contraseña incorrecta.".encode())
     else:
         cliente_socket.send("Error: Correo no encontrado.".encode())
 
-# Manejar sesiones individuales de cada cliente conectado
 def manejar_sesiones(cliente_socket, email):
     while True:
-        opcion = cliente_socket.recv(1024).decode().strip()
-        if opcion == "1":
+        solicitud = cliente_socket.recv(1024).decode().strip()
+        if solicitud == "1":
             nueva_contraseña = cliente_socket.recv(1024).decode().strip()
             clientes_db[email]["password"] = nueva_contraseña
+            guardar_db()
             cliente_socket.send("Contraseña cambiada exitosamente.".encode())
-        elif opcion == "7":
+        elif solicitud == "7":
             cliente_socket.send("Desconectado.".encode())
             break
         else:
@@ -45,11 +54,9 @@ def iniciar_servidor():
     servidor.bind(("localhost", 9999))
     servidor.listen(10)
     print("[SERVIDOR] Servidor iniciado y esperando conexiones...")
-
     while True:
         cliente_socket, direccion = servidor.accept()
         threading.Thread(target=manejar_cliente, args=(cliente_socket, direccion)).start()
 
 if __name__ == "__main__":
     iniciar_servidor()
-
